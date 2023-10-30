c9893e2c6e07c1bbe0337a4edf2e80e26e4b7df1
J27avier <salesortiz@gmail.com>
2023-10-29 20:31:05 -0600

    Debug singlelayer

--- EvGym/charge_rl_agent.py
+++ EvGym/charge_rl_agent.py
@@ -21,25 +21,24 @@ def layer_init(layer, std=np.sqrt(2), bias_const =0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
-# Not needed
-#def bounds_from_obs(obs):
-#    np_obs = obs.cpu().numpy()
-#    data_state = np_obs[0, :config.max_cars*4]
-#    df_state = pd.DataFrame(data = data_state.reshape((config.max_cars, 4)), columns = ["soc_t", "t_rem", "soc_dis", "t_dis"])
-#
-#    occ_spots = df_state["t_rem"] > 0 # Occupied spots
-#
-#    hat_y_low = config.FINAL_SOC-df_state["soc_t"] - config.alpha_c*config.eta_c*(df_state["t_rem"] - 1)/config.B
-#    y_low = np.minimum(hat_y_low * config.eta_c, hat_y_low / copnfig.eta_d)
-#    y_low[~occ_spots] = 0
-#
-#    lower = np.maximum(y_low, np.maximum(-config.alpha_d/config.B,
-#                                              -df_state["soc_dis"]))
-#    lower[~occ_spots] = 0
-#
-#    upper_soc = (config.FINAL_SOC - df_state["soc_t"]) / config.eta_c
-#    upper = np.minimum(upper_soc,  config.alpha_c / config.B)
-#    upper[~occ_spots] = 0
+def bounds_from_obs(obs, state=False):
+    np_obs = obs.cpu().numpy()
+    data_state = np_obs[0, :config.max_cars*4]
+    df_state = pd.DataFrame(data = data_state.reshape((config.max_cars, 4)), columns = ["soc_t", "t_rem", "soc_dis", "t_dis"])
+
+    occ_spots = df_state["t_rem"] > 0 # Occupied spots
+
+    hat_y_low = config.FINAL_SOC-df_state["soc_t"] - config.alpha_c*config.eta_c*(df_state["t_rem"] - 1)/config.B
+    y_low = np.minimum(hat_y_low * config.eta_c, hat_y_low / copnfig.eta_d)
+    y_low[~occ_spots] = 0
+
+    lower = np.maximum(y_low, np.maximum(-config.alpha_d/config.B,
+                                              -df_state["soc_dis"]))
+    lower[~occ_spots] = 0
+
+    upper_soc = (config.FINAL_SOC - df_state["soc_t"]) / config.eta_c
+    upper = np.minimum(upper_soc,  config.alpha_c / config.B)
+    upper[~occ_spots] = 0
 
 
 class Safe_Actor_Mean_Agg(nn.Module):
@@ -345,6 +344,7 @@ class agentPPO_lay(nn.Module):
         return action, logprob, entropy, value 
 
     def action_to_env(self, action):
+        input()
         return action.cpu().numpy().squeeze()
 
 
