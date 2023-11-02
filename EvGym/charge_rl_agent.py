79e5c29bb236a1d60eb8f6bcbbffee59647fbf51
J27avier <salesortiz@gmail.com>
2023-11-01 23:30:56 -0600

    Experiments to git

--- EvGym/charge_rl_agent.py
+++ EvGym/charge_rl_agent.py
@@ -21,8 +21,8 @@ from .safety_layer import SafetyLayer, SafetyLayerAgg
 from .charge_utils import bounds_from_obs
 
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-    #torch.nn.init.orthogonal_(layer.weight, std)
-    torch.nn.init.normal_(layer.weight, 1, std)
+    torch.nn.init.orthogonal_(layer.weight, std)
+    #torch.nn.init.normal_(layer.weight, 1, std)
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
--- EvGym/charge_utils.py
+++ EvGym/charge_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+import argparse
 
 from . import config
 
