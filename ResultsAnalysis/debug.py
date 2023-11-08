9b16177a8f278e3d4b5eddef903dd94cedb43634
J27avier <salesortiz@gmail.com>
2023-11-08 00:47:29 -0700

    Organized experiments

--- ResultsAnalysis/debug.py
+++ ResultsAnalysis/debug.py
@@ -1,5 +1,10 @@
 import torch
-import pyfiglet
-a = 10
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--a", help="Type of a", type=str, required=True)
+args = parser.parse_args()
+
+a = args.a
 a += 1
 print(a)
