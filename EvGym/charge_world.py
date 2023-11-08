f54a8d7d6b34761be634acc56fd7d76d7aaa3c80
J27avier <salesortiz@gmail.com>
2023-11-08 02:33:17 -0700

    Denormalize reward

--- EvGym/charge_world.py
+++ EvGym/charge_world.py
@@ -117,8 +117,8 @@ class ChargeWorldEnv():
         occ_spots = self.df_park["idSess"] != -1 # Occupied spots
         n_cars = occ_spots.sum()
         #norm = (config.FINAL_SOC - self.df_park[occ_spots]["soc_t"]).sum()
-        return -self.imb_transf/n_cars if n_cars > 0 else 0 #and norm > 0 else 0
-        #return -self.imb_transf
+        #return -self.imb_transf/n_cars if n_cars > 0 else 0 #and norm > 0 else 0
+        return -self.imb_transf
 
     def _cars_depart(self):
         blank_session = Session()
