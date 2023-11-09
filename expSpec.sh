856d440f94e6a195d392439e34269d7465e82db0
J27avier <salesortiz@gmail.com>
2023-11-09 03:40:02 +0000

    Experiment results

--- expSpec.sh
+++ expSpec.sh
@@ -1,2 +1,2 @@
-python3 RunRLChargeWorld.py --agent PPO-agg --save-name month_agg_ecos --save-agent --reward-coef 1 --proj-coef 0 --file-price df_price_2019_pad.csv --num-steps 24 --years 10 --anneal-lr True --update-epochs 10 --num-minibatches 1  && echo "Done month_agg!" &
+python3 RunRLChargeWorld.py --agent PPO-agg --save-name month_agg_ecos --save-agent --reward-coef 1 --proj-coef 0 --file-price df_price_2019_pad.csv --num-steps 24 --years 10 --anneal-lr True --update-epochs 10 --num-minibatches 2  && echo "Done month_agg!" &
 
