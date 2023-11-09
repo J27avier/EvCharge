f62e1919c78f85de9f2b3d061bb3bb9aa85adb44
J27avier <salesortiz@gmail.com>
2023-11-09 16:43:16 -0700

    Hardcode std

--- EvGym/charge_rl_agent.py
+++ EvGym/charge_rl_agent.py
@@ -176,6 +176,7 @@ class agentPPO_agg(nn.Module):
         action_std = torch.exp(action_logstd) / 30
         probs = Normal(action_mean, action_std)
         if action is None:
+            probs = Normal(action_mean, (self.sum_upper-self.sum_lower)/10)
             #print(f"{action_mean=}, {action_mean.shape=}, {type(action_mean)=}")
             action_t = probs.sample()
             # Double safety
