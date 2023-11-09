b2413ba99b8deeb7e761414b706fd3a417d5125b
J27avier <salesortiz@gmail.com>
2023-11-09 02:07:28 +0000

    New init with normal

--- expSpec.sh
+++ expSpec.sh
@@ -1,7 +1,2 @@
-python3 RunRLChargeWorld.py --agent PPO-agg --save-name month_agg_norma --save-agent --reward-coef 1 --proj-coef 0 --learning-rate 0.0003 --file-price df_price_2019_pad.csv --num-steps 24 --years 50 --anneal-lr True  --update-epochs 10 --num-minibatches 8  && echo "Done month_agg!" &
+python3 RunRLChargeWorld.py --agent PPO-agg --save-name month_agg_init_norm --save-agent --reward-coef 1 --proj-coef 0 --file-price df_price_2019_pad.csv --num-steps 24 --years 10 --anneal-lr True  --update-epochs 10 --num-minibatches 1  && echo "Done month_agg!" &
 
-python3 RunRLChargeWorld.py --agent PPO-agg --save-name month_agg_norma_moresteps --save-agent --reward-coef 1 --proj-coef 0 --learning-rate 0.0001 --file-price df_price_2019_pad.csv --num-steps 72 --years 50 --anneal-lr True  --update-epochs 10 --num-minibatches 8  && echo "Done month_agg!" &
-
-python3 RunRLChargeWorld.py --agent PPO-lay --save-name month_ind_norma --save-agent --reward-coef 1 --proj-coef 0 --learning-rate 0.0003 --file-price df_price_2019_pad.csv --num-steps 24 --years 50 --anneal-lr True  --update-epochs 10 --num-minibatches 8  && echo "Done month_agg!" &
-
-python3 RunRLChargeWorld.py --agent PPO-lay --save-name month_ind_norma_moresteps --save-agent --reward-coef  1 --proj-coef 0 --learning-rate 0.0001 --file-price df_price_2019_pad.csv --num-steps 72 --years 50 --anneal-lr True  --update-epochs 10 --num-minibatches 8  && echo "Done month_agg!" &
