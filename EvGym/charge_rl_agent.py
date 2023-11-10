8c7fcc44ae04fc9dade784a32606b62fa646d4cb
J27avier <salesortiz@gmail.com>
2023-11-10 03:27:08 -0700

    asdf

--- EvGym/charge_rl_agent.py
+++ EvGym/charge_rl_agent.py
@@ -199,12 +199,12 @@ class agentPPO_agg(nn.Module):
         return df_state_simpl, pred_price, hour
 
     def _get_action_and_value(self, x,  action=None):
-        #if x.ndim == 1:
-        #    action_mean, proj_loss = self.actor_mean(x)
-        #    action_mean = action_mean.unsqueeze(1)
-        #else:
-        #    action_mean, proj_loss = self.actor_mean(x)
-        action_mean, proj_loss = self.actor_mean(x)
+        if x.ndim == 1:
+            action_mean, proj_loss = self.actor_mean(x)
+            action_mean = action_mean.unsqueeze(1)
+        else:
+            action_mean, proj_loss = self.actor_mean(x)
+        #action_mean, proj_loss = self.actor_mean(x)
         self.proj_loss = proj_loss.cpu().numpy().squeeze()
         action_logstd = self.actor_logstd.expand_as(action_mean)
         action_std = torch.exp(action_logstd) #/ 10
