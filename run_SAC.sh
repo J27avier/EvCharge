b277c5056ec948bcfe81d44db29a0ac18dcbc353
J27avier <salesortiz@gmail.com>
2023-11-17 03:32:17 +0000

    New exp

--- run_SAC.sh
+++ run_SAC.sh
@@ -1,13 +1,13 @@
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_b --years 100 --state-rep noprm --n-state 39 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_c --years 100 --state-rep oprm --n-state 39 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_d --years 100 --state-rep orm --n-state 19 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_e --years 100 --state-rep norm --n-state 19 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_f --years 100 --state-rep om --n-state 19 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_g --years 100 --state-rep m --n-state 13 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_h --years 100 --norm-reward --state-rep noprm --n-state 39 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_i --years 100 --norm-reward --state-rep oprm --n-state 39 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_j --years 100 --norm-reward --state-rep orm --n-state 19 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_k --years 100 --norm-reward --state-rep norm --n-state 19 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_l --years 100 --norm-reward --state-rep om --n-state 19 &
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_I_m --years 100 --norm-reward --state-rep m --n-state 13 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_b --years 100 --state-rep noprm --n-state 39 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_c --years 100 --state-rep oprm --n-state 39 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_d --years 100 --state-rep orm --n-state 19 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_e --years 100 --state-rep norm --n-state 19 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_f --years 100 --state-rep om --n-state 19 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_g --years 100 --state-rep m --n-state 13 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_h --years 100 --norm-reward --state-rep noprm --n-state 39 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_i --years 100 --norm-reward --state-rep oprm --n-state 39 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_j --years 100 --norm-reward --state-rep orm --n-state 19 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_k --years 100 --norm-reward --state-rep norm --n-state 19 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_l --years 100 --norm-reward --state-rep om --n-state 19 &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_II_m --years 100 --norm-reward --state-rep m --n-state 13 &
 
