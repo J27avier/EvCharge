2a3d4fd74daa59feb42013db80c178959c22b542
J27avier <salesortiz@gmail.com>
2023-12-06 01:18:52 +0000

    Noise in prices

--- run_Gen.sh
+++ run_Gen.sh
@@ -1,1 +1,1 @@
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_a --years 100 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_b --years 100 --general True &
