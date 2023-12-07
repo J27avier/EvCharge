300229d999eef383772cd95c52258bcabc7bb901
J27avier <salesortiz@gmail.com>
2023-12-07 02:49:13 +0000

    asdf

--- run_Gen.sh
+++ run_Gen.sh
@@ -1,2 +1,2 @@
-nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_d --years 100 --general True && echo "D done" >> notify.txt &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_e --years 50 --general True && echo "E done" >> notify.txt &
 #nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_d --years 100 --general True && echo "D done" >> notify.txt &
