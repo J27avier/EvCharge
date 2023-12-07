254f240ab137c71b89e7d1bb077fd9ffd336ac3a
J27avier <salesortiz@gmail.com>
2023-12-07 23:07:13 +0000

    Generalization results

--- run_Gen.sh
+++ run_Gen.sh
@@ -1,2 +1,33 @@
-python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_f --years 50 --general True && echo "F done" >> notify.txt &
-#nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_d --years 100 --general True && echo "D done" >> notify.txt &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_a --years 500 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_b --years 500 --disagg LL --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_c --years 500 --batch-size 128 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_d --years 500 --batch-size 512 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_e --years 500 --learning-starts 0 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_f --years 500 --learning-starts 72 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_g --years 500 --gamma 1.00 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_h --years 500 --gamma 0.95 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_i --years 500 --gamma 0.90 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_j --years 500 --tau 0.1 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_k --years 500 --tau 0.01 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_l --years 500 --tau 0.001 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_m --years 500 --tau 0.0001 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_n --years 500 --alpha 0.1 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_o --years 500 --alpha 0.3 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_p --years 500 --alpha 0.4 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_q --years 500 --alpha 0.5 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_r --years 500 --rng-test True --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_s --years 500 --price-noise 1 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_t --years 500 --rng-test True --price-noise 1 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_u --years 500 --price-noise 3 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_v --years 500 --rng-test True --price-noise 3  --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_w --years 500 --price-noise 10 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_x --years 500 --rng-test True --price-noise 10 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_y --years 500 --rng-test True --price-noise 3 --tau 0.01 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_I_z --years 500 --rng-test True --price-noise 3 --alpha 0.4 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_a --years 500 --policy-lr 0.0001 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_b --years 500 --policy-lr 0.001 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_c --years 500 --q-lr 0.0003 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_d --years 500 --q-lr 0.003 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_e --years 500 --policy-frequency 1 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_f --years 500 --policy-frequency 4 --general True &
+nohup python3 RunSACChargeWorld.py --agent SAC-sagg --save-name month_sac_gen_J_g --years 500 --target-network-frequency 2 --general True &
