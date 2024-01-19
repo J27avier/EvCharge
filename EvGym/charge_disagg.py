5f88d1501408e2be9839a613379cc1a34f89bdee
J27avier <salesortiz@gmail.com>
2024-01-19 12:08:04 -0700

    More figures

--- EvGym/charge_disagg.py
+++ EvGym/charge_disagg.py
@@ -53,17 +53,19 @@ def proportionalFairness(Y_tot, lower, upper, occ_spots):
         lower = lower.to_numpy(dtype = np.float64)
 
         ev_ranges = (upper - lower)
-        bool_ranges = np.array([not isclose(ev_range, 0, abs_tol = 1e-05) for ev_range in ev_ranges]) #
+        #bool_ranges = np.array([not isclose(ev_range, 0, abs_tol = 1e-05) for ev_range in ev_ranges]) #
+        bool_ranges = np.array([not isclose(ev_range, 0) for ev_range in ev_ranges]) #
         to_disagg = bool_ranges & occ_spots
         n = sum(to_disagg)
         Y_surplus = (Y_tot - lower.sum())
 
-
+    
         if n > 0:
             Y = cp.Variable(n)
             s_lower = lower[to_disagg] # Subset lower
             s_upper = upper[to_disagg] # Subset upper
             s_range = s_upper - s_lower
+
 
             if Y_surplus > s_range.sum():
                 #print(f"Warning: surplus larger by: {Y_surplus - s_range.sum()}")
@@ -71,39 +73,44 @@ def proportionalFairness(Y_tot, lower, upper, occ_spots):
                     raise "Discrepancy too large"
                 Y_surplus = s_range.sum()
 
-            constraints = []
-            constraints += [0 <= Y]
-            constraints += [Y <= s_range]
-            constraints += [cp.sum(Y) == Y_surplus]
+            if all((Y_surplus/n) < s_range):
+                y_sur_indiv = np.ones(len(s_range))*Y_surplus/n
 
-            objective = cp.Maximize(cp.sum(cp.log(Y+1)))
-            prob = cp.Problem(objective, constraints)
+            else:
+                constraints = []
+                constraints += [0 <= Y]
+                constraints += [Y <= s_range]
+                constraints += [cp.sum(Y) <= Y_surplus]
 
-            # Custom error message
-            try:
-                prob.solve(cp.MOSEK, mosek_params = {'MSK_IPAR_NUM_THREADS': 8, 'MSK_IPAR_BI_MAX_ITERATIONS': 2_000_000, "MSK_IPAR_INTPNT_MAX_ITERATIONS": 800}, verbose=False)  
-                if prob.status != 'optimal':
-                    print(prob.status)
-                    raise Exception("Optimal disaggregation not found")
-            except Exception:
-                print("----")
-                print("low", [f"{x:.4f}" for x in lower])
-                print("upp", [f"{x:.4f}" for x in upper])
-                print(n)
-                print("ran", [f"{x:.4f}" for x in (s_range)], s_range.sum())
-                print(Y_surplus)
-                print("Error in proportional fairness disaggregation")
-                #raise SystemExit('Error in proportional fairness disaggregation.')
+                objective = cp.Maximize(cp.sum(cp.log(Y+1)))
+                prob = cp.Problem(objective, constraints)
+
+                # Custom error message
+                try:
+                    prob.solve(cp.MOSEK, mosek_params = {'MSK_IPAR_NUM_THREADS': 8, 'MSK_IPAR_BI_MAX_ITERATIONS': 2_000_000, "MSK_IPAR_INTPNT_MAX_ITERATIONS": 800}, verbose=False)  
+                    if prob.status != 'optimal':
+                        print(prob.status)
+                        raise Exception("Optimal disaggregation not found")
+                except Exception:
+                    print("----")
+                    print("low", [f"{x:.4f}" for x in lower])
+                    print("upp", [f"{x:.4f}" for x in upper])
+                    print(n)
+                    print("ran", [f"{x:.4f}" for x in (s_range)], s_range.sum())
+                    print(Y_surplus)
+                    print("Error in proportional fairness disaggregation")
+                    #raise SystemExit('Error in proportional fairness disaggregation.')
+                y_sur_indiv = Y.value
 
         y = lower.copy()
         j = 0
         for i in range(len(lower)):
             if to_disagg[i]:
-                y[i] += Y.value[j]
+                y[i] += y_sur_indiv[j]
                 j += 1
     except Exception:
         print("WARNING! proportionalFairness failed")
         y = proportional(Y_tot, lower, upper, occ_spots)
         #Super hacky way to track errors
-        os.system("echo 'err' >> err_disagg.txt")
+        os.system("echo 'err' >> lerr_disagg.txt")
     return y
